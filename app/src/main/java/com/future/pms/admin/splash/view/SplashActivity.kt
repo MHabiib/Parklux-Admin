239de4b4e65eb4c9e37e969f00328a3017088230
Muhammad Habib <muhammad.habib@gdn-commerce.com>
2020-01-24 16:06:24 +0700

    #29 update splash screen

--- app/src/main/java/com/future/pms/admin/splash/view/SplashActivity.kt
+++ app/src/main/java/com/future/pms/admin/splash/view/SplashActivity.kt
@@ -2,13 +2,17 @@ package com.future.pms.admin.splash.view
 
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.future.pms.admin.BaseApp
 import com.future.pms.admin.R
 import com.future.pms.admin.core.base.BaseActivity
+import com.future.pms.admin.databinding.ActivitySplashBinding
 import com.future.pms.admin.login.view.LoginActivity
 import com.future.pms.admin.main.view.MainActivity
 import com.future.pms.admin.splash.injection.DaggerSplashComponent
@@ -26,26 +30,36 @@ class SplashActivity : BaseActivity(), SplashContract {
   }
 
   @Inject lateinit var presenter: SplashPresenter
+  private lateinit var binding: ActivitySplashBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
     val settings = getSharedPreferences("prefs", 0)
     val firstRun = settings.getBoolean("firstRun", false)
     if (!firstRun) {
-      setContentView(R.layout.activity_splash)
       presenter.attach(this)
-      initView()
+      if (isOnline()) {
+        presenter.isAuthenticated()
+      } else {
+        binding.ibRefresh.visibility = View.VISIBLE
+        binding.progressBar.visibility = View.GONE
+        binding.ibRefresh.setOnClickListener {
+          if (isOnline()) {
+            binding.ibRefresh.visibility = View.GONE
+            binding.progressBar.visibility = View.VISIBLE
+            presenter.isAuthenticated()
+          } else {
+            Toast.makeText(this, R.string.no_network_connection, Toast.LENGTH_LONG).show()
+          }
+        }
+      }
     } else {
       val a = Intent(this, Dispatchers.Main::class.java)
       startActivity(a)
     }
     window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
         WindowManager.LayoutParams.FLAG_FULLSCREEN)
-    setContentView(R.layout.activity_splash)
-    presenter.attach(this)
-  }
-
-  private fun initView() {
-    presenter.isAuthenticated()
   }
 
   override fun onSuccess() {
@@ -70,6 +84,12 @@ class SplashActivity : BaseActivity(), SplashContract {
       startActivity(intent)
       finish()
     }, 5000)
+  }
+
+  private fun isOnline(): Boolean {
+    val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+    val activeNetwork = cm?.activeNetworkInfo
+    return activeNetwork != null && activeNetwork.isConnected
   }
 
   override fun onFailed(message: String) {
